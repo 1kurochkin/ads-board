ae7a6a678e91f273434a2e5cefc1dc89167713a7
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-03 23:55:14 +0300

    Создал selectItem компонент

---
+++ src/components/searchBox/select/selectItem/selectItem.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./selectItem.css"
+
+type PropsType = {
+    label: string
+    isActive: any
+    onChangeHandler: any
+    className?: string
+}
+
+const SelectItem = (props: PropsType) => {
+
+    const {label, isActive, onChangeHandler, className} = props
+
+    return (
+        <div onClick={onChangeHandler} className={`selectItem ${className}`}>
+            <div className="selectItem__label">{label}</div>
+            <input checked={isActive} type="radio" className="selectItem__checkBox"/>
+        </div>
+    );
+}
+
+export default SelectItem;
