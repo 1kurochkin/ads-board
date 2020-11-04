fbca8b9f3cd17c9260146186170c9496a0f17c14
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-04 19:15:18 +0300

    Создал thunks для SearchBox компонента

--- src/redux/thunks/thunks.tsx
+++ src/redux/thunks/thunks.tsx
@@ -5,9 +5,15 @@ import {AppStateType} from "../store";
 import {
     setIsAuthAC,
     SetIsAuthACType,
-    setIsCorrectAuthDataAC, SetIsRegistrationACType,
+    setIsCorrectAuthDataAC,
 } from "../reducers/authorizationState/authorizationStateActionCreators";
 import {CallHistoryMethodAction, push} from "connected-react-router";
+import {
+    setSearchedDataAC,
+    SetSearchedDataAType,
+    setSubwayStationsDataAC,
+    SetSubwayStationsDataACType, setTotalNumOfPageSearchReducerAC, SetTotalNumOfPageSearchReducerACType
+} from "../reducers/searchBoxState/searchBoxStateActionCreators";
 
 type GetStateType = () => AppStateType
 
@@ -34,5 +40,37 @@ export const sendAuthorizationOrRegistrationThunk = (data:AuthorizationData & Re
                 dispatch(push("/feed"))
             } else dispatch(setIsCorrectAuthDataAC(false))
         })
+        .catch(err => console.error(err))
+}
+
+//--------------------SEARCH-BOX---------------------//
+type GetSubwayStationsDispatchType = Dispatch<SetSubwayStationsDataACType>
+export const getSubwayStationsThunk = () => (dispatch: GetSubwayStationsDispatchType) => {
+    console.log("getSubwayStationsThunk")
+
+    testAPI["getSubwayStations"]()
+        .then(response => {
+            if(response.status !== 200)
+                throw `CANNOT FETCH GET REQUEST ${response.status}`
+            return response.json()
+        })
+        .then( data => { dispatch(setSubwayStationsDataAC(data)) } )
+        .catch(err => console.error(err))
+}
+
+type GetAnnouncementsByFiltersDispatchType = Dispatch<SetSearchedDataAType | SetTotalNumOfPageSearchReducerACType>
+export const getAnnouncementsByFiltersThunk = (name:string = "", category:string| number = "", subway:string| number = "", withConcat:boolean = false) => (dispatch: GetAnnouncementsByFiltersDispatchType, getState: any) => {
+    console.log("getAnnouncementsByFiltersThunk")
+    const {currentPage} = getState().searchBoxState
+    testAPI["getAnnouncementsByFilters"](currentPage, name, category, subway)
+        .then(response => {
+            if(response.status !== 200)
+                throw `CANNOT FETCH GET REQUEST ${response.status}`
+            return response.json()
+        })
+        .then( ({totalNumOfPages, announcements}) => {
+            dispatch(setTotalNumOfPageSearchReducerAC(totalNumOfPages))
+            dispatch(setSearchedDataAC(announcements, withConcat))
+        } )
         .catch(err => console.error(err))
 }
