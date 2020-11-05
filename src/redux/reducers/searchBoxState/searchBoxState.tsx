fe30799ed4542a03d68ce2f14045f2c395224e5f
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-05 22:51:47 +0300

    Рефакторинг модели searchBox - изменил наименование для action isFetching

--- src/redux/reducers/searchBoxState/searchBoxState.tsx
+++ src/redux/reducers/searchBoxState/searchBoxState.tsx
@@ -1,11 +1,9 @@
 export const SET_SEARCHED_DATA = "SET_SEARCHED_DATA"
 export const SET_SUBWAY_STATIONS_DATA = "SET_SUBWAY_STATIONS_DATA"
-export const SET_IS_FETCHING = "SET_IS_FETCHING"
+export const SET_IS_FETCHING_SEARCH_REDUCER = "SET_IS_FETCHING_SEARCH_REDUCER"
 export const SET_CURRENT_PAGE_SEARCH_REDUCER = "SET_CURRENT_PAGE_SEARCH_REDUCER"
 export const SET_TOTAL_NUM_OF_PAGE_SEARCH_REDUCER = "SET_TOTAL_NUM_OF_PAGE_SEARCH_REDUCER"
-// export const SET_SEARCH_CONFIG_CATEGORY_ID = "SET_SEARCH_CONFIG_CATEGORY_ID"
-// export const SET_SEARCH_CONFIG_SUBWAY_STATIONS = "SET_SEARCH_CONFIG_SUBWAY_STATIONS"
-// export const SET_SEARCH_CONFIG_SEARCH_VALUE = "SET_SEARCH_CONFIG_SEARCH_VALUE"
+
 
 export type initialStateType = typeof initialState
 const initialState = {
@@ -25,12 +23,7 @@ const initialState = {
             ]
         },
         {id: 2, label: "Работа", subCategories: []}
-    ],
-    // searchConfig : {
-    //     categoryId: null,
-    //     subwayStations: {id: 0, label: "Станция метро"},
-    //     searchValue: ""
-    // }
+    ]
 }
 
 export const searchBoxStateReducer = (state = initialState, action: any): initialStateType => {
@@ -45,10 +38,9 @@ export const searchBoxStateReducer = (state = initialState, action: any): initia
             return {...state, searchedData: newSearchedData}
         case SET_SUBWAY_STATIONS_DATA :
             console.log("SET_SUBWAY_STATIONS_DATA", value)
-            const newSubwayStationsData = subwayStationsData.concat(value)
-            return {...state, subwayStationsData: newSubwayStationsData}
-        case SET_IS_FETCHING :
-            console.log("SET_IS_FETCHING", value)
+            return {...state, subwayStationsData: value}
+        case SET_IS_FETCHING_SEARCH_REDUCER :
+            console.log("SET_IS_FETCHING_SEARCH_REDUCER", value)
             return {...state, isFetching: value}
         case SET_CURRENT_PAGE_SEARCH_REDUCER :
             const newCurrentPage = currentPage + 1
@@ -57,18 +49,6 @@ export const searchBoxStateReducer = (state = initialState, action: any): initia
         case SET_TOTAL_NUM_OF_PAGE_SEARCH_REDUCER :
             console.log("SET_TOTAL_NUM_OF_PAGE_SEARCH_REDUCER", value)
             return {...state, totalNumOfPages: value}
-        // case SET_SEARCH_CONFIG_CATEGORY_ID :
-        //     console.log("SET_SEARCH_CONFIG_CATEGORY_ID", value)
-        //     return {...state, searchConfig: {...searchConfig, categoryId: value}}
-        // case SET_SEARCH_CONFIG_SUBWAY_STATIONS :
-        //     console.log("SET_SEARCH_CONFIG_SUBWAY_STATIONS", value)
-            // @ts-ignore
-            // const newSubwayStations = subwayStations.includes(value) ?
-            //     subwayStations.filter(el => el !== value) : subwayStations.concat(value)
-            // return {...state, searchConfig: {...searchConfig, subwayStations: value}}
-        // case SET_SEARCH_CONFIG_SEARCH_VALUE :
-        //     console.log("SET_SEARCH_CONFIG_SEARCH_VALUE", value)
-        //     return {...state, searchConfig: {...searchConfig, searchValue: value}}
         default: return state
     }
 }
--- src/redux/reducers/searchBoxState/searchBoxStateActionCreators.tsx
+++ src/redux/reducers/searchBoxState/searchBoxStateActionCreators.tsx
@@ -1,19 +1,19 @@
 import {
     SET_SEARCHED_DATA,
-    SET_IS_FETCHING,
+    SET_IS_FETCHING_SEARCH_REDUCER,
     SET_CURRENT_PAGE_SEARCH_REDUCER,
     SET_TOTAL_NUM_OF_PAGE_SEARCH_REDUCER,
-    SET_SUBWAY_STATIONS_DATA,
+    SET_SUBWAY_STATIONS_DATA
 } from "./searchBoxState";
 
 export const setSearchedDataAC = (value:Array<any>, withConcat:boolean):SetSearchedDataAType => ({ type: SET_SEARCHED_DATA, payload: {value, withConcat} })
 export const setSubwayStationsDataAC = (value:Array<any>):SetSubwayStationsDataACType => ({ type: SET_SUBWAY_STATIONS_DATA, payload: {value} })
-export const setIsFetchingAC = (value:boolean):SetIsFetchingACType => ({ type: SET_IS_FETCHING, payload: {value} })
+export const setIsFetchingAC = (value:boolean):SetIsFetchingACType => ({ type: SET_IS_FETCHING_SEARCH_REDUCER, payload: {value} })
 export const setCurrentPageSearchReducerAC = (value:number):SetCurrentPageSearchReducerACType => ({ type: SET_CURRENT_PAGE_SEARCH_REDUCER, payload: {value} })
 export const setTotalNumOfPageSearchReducerAC = (value:number):SetTotalNumOfPageSearchReducerACType => ({ type: SET_TOTAL_NUM_OF_PAGE_SEARCH_REDUCER, payload: {value} })
 
 export type SetSearchedDataAType = { type: typeof SET_SEARCHED_DATA, payload: { value: Array<any>, withConcat: boolean}}
 export type SetSubwayStationsDataACType = { type: typeof SET_SUBWAY_STATIONS_DATA, payload: { value: Array<any> }}
-export type SetIsFetchingACType = { type: typeof SET_IS_FETCHING, payload: { value: boolean }}
+export type SetIsFetchingACType = { type: typeof SET_IS_FETCHING_SEARCH_REDUCER, payload: { value: boolean }}
 export type SetCurrentPageSearchReducerACType = { type: typeof SET_CURRENT_PAGE_SEARCH_REDUCER, payload: { value: number } }
 export type SetTotalNumOfPageSearchReducerACType = { type: typeof SET_TOTAL_NUM_OF_PAGE_SEARCH_REDUCER, payload: { value: number } }
