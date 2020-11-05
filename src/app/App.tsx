a000db1977ee11a309b3ea13e9ef1bff1bc7a3d4
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-05 22:55:56 +0300

    Добавил в App роутинги для: FeedPage,  ContactsPage, SettingsPage, AnnouncementPage, MyAnnouncementsPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import "./appStyles.css"
-import ModalWindow from "../components/modalWindow/modalWindow";
-import AuthorizationPopupBox from "../components/modalWindow/authorizationPopupBox/authorizationPopupBox";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import FeedPage from "../pages/feedPage/feedPage";
+import ContactsPage from "../pages/contactsPage/contactsPage";
+import SettingsPage from "../pages/settingsPage/settingsPage";
+import AnnouncementPage from '../pages/announcementPage/announcementPage';
+import MyAnnouncementsPage from "../pages/myAnnouncementsPage/myAnnouncementsPage";
 
 const App = () => {
 
   return (
     <div className="App">
-        <ModalWindow isActiveFromProps={true}>
-            <AuthorizationPopupBox/>
-        </ModalWindow>
+        <Switch>
+            <Route exact path="/feed" component={FeedPage}/>
+            <Route exact path="/myAnnouncements" component={MyAnnouncementsPage}/>
+            <Route exact path="/settings" component={SettingsPage}/>
+            <Route exact path="/contacts" component={ContactsPage}/>
+            <Route exact path="/housing" component={FeedPage}/>
+            <Route exact path="/job" component={FeedPage}/>
+            <Route exact path="/:category" component={FeedPage}/>
+            <Route exact path="/:category/:id" component={AnnouncementPage}/>
+            <Route exact path="/createAnnouncement" component={ContactsPage}/>
+            <Redirect to="/feed"/>
+        </Switch>
     </div>
   );
 }
