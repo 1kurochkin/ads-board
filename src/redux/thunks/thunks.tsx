bacb1ba304a91ac7bd9bffb65c00369d9863e607
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-05 22:37:23 +0300

    Создал thunks для myAnnouncementsPage, announcementPage, feedPage, settingsPage

--- src/redux/thunks/thunks.tsx
+++ src/redux/thunks/thunks.tsx
@@ -3,6 +3,8 @@ import {setIsSendingDataAC, SetIsSendingDataACType} from "../reducers/mainState/
 import {Dispatch} from "redux";
 import {AppStateType} from "../store";
 import {
+    logoutOrDeleteUseAC,
+    LogoutOrDeleteUserACType,
     setIsAuthAC,
     SetIsAuthACType,
     setIsCorrectAuthDataAC,
@@ -12,8 +14,35 @@ import {
     setSearchedDataAC,
     SetSearchedDataAType,
     setSubwayStationsDataAC,
-    SetSubwayStationsDataACType, setTotalNumOfPageSearchReducerAC, SetTotalNumOfPageSearchReducerACType
+    SetSubwayStationsDataACType,
+    setTotalNumOfPageSearchReducerAC,
+    SetTotalNumOfPageSearchReducerACType
 } from "../reducers/searchBoxState/searchBoxStateActionCreators";
+import {
+    SetetIsFetchingFeedReducerACType,
+    setIsFetchingFeedReducerAC,
+    setLastAnnouncementsAC,
+    SetLastAnnouncementsACType
+} from "../reducers/feedState/feedStateActionCreators";
+import {SettingsFieldType} from "../reducers/settingsState/settingsState";
+import {
+    setSettingsInLocalStorageByFieldAC,
+    SetSettingsInLocalStorageByFieldACType
+} from "../reducers/settingsState/settingsStateActionCreators";
+import {
+    SettIsFetchingAnnouncementReducerACType,
+    SetAnnouncementsACType,
+    settIsFetchingAnnouncementReducerAC,
+    setAnnouncementAC
+} from "../reducers/announcementState/announcementStateActionCreators";
+import {
+    setMyAnnouncementsAC,
+    SetMyAnnouncementsACType,
+    SettIsFetchingMyAnnouncementReducerACType,
+    settIsFetchingMyAnnouncementsReducerAC, setTotalNumOfPageMyAnnouncementReducerAC,
+    SetTotalNumOfPageMyAnnouncementReducerACType,
+    DelMyAnnouncementByIdACType, delMyAnnouncementByIdAC
+} from "../reducers/myAnnouncementState/myAnnouncementStateActionCreators";
 
 type GetStateType = () => AppStateType
 
@@ -72,5 +101,126 @@ export const getAnnouncementsByFiltersThunk = (name:string = "", category:string
             dispatch(setTotalNumOfPageSearchReducerAC(totalNumOfPages))
             dispatch(setSearchedDataAC(announcements, withConcat))
         } )
+        .catch(err => console.error(err))
+}
+
+//--------------------FEED-PAGE--------------------//
+type getLastAnnouncementsDispatchType = Dispatch<SetetIsFetchingFeedReducerACType | SetLastAnnouncementsACType>
+export const getLastAnnouncementsThunk = () => (dispatch: getLastAnnouncementsDispatchType) => {
+    dispatch(setIsFetchingFeedReducerAC(true))
+    console.log("sendAuthorizationOrRegistrationThunk")
+
+    testAPI["getLastAnnouncements"]()
+        .then(response => {
+            if(response.status !== 200)
+                throw `CANNOT FETCH GET REQUEST ${response.status}`
+            return response.json()
+        })
+        .then( (data)  => {
+            dispatch(setIsFetchingFeedReducerAC(false))
+            dispatch(setLastAnnouncementsAC(data))
+        })
+        .catch(err => console.error(err))
+}
+//--------------------SETTINGS-PAGE--------------------//
+type PostSettingByFieldDispatchType = Dispatch<SetSettingsInLocalStorageByFieldACType>
+export const postSettingByFieldThunk = (data: any, field: SettingsFieldType) => (dispatch: PostSettingByFieldDispatchType) => {
+    console.log("postSettingByFieldThunk")
+    const [value] = Object.values(data)
+
+    testAPI["postSettingsByField"](data, field)
+        .then(response => {
+            if(response.status !== 200)
+                throw `CANNOT FETCH POST REQUEST ${response.status}`
+            return response.json()
+        })
+        .then( ({result = false})  => {
+            result && dispatch(setSettingsInLocalStorageByFieldAC(value, field))
+        })
+        .catch(err => console.error(err))
+}
+
+type PostLogoutOrDeleteUserType = Dispatch<LogoutOrDeleteUserACType | CallHistoryMethodAction>
+type LogoutOrDeleteType = "logout" | "delete"
+export const postLogoutOrDeleteUser = (logoutOrDelete: LogoutOrDeleteType) => (dispatch: PostLogoutOrDeleteUserType) => {
+    console.log("postLogoutOrDeleteUser")
+
+    const getRequestByLogoutOrDelete = () => {
+        if(logoutOrDelete === "logout") return "postLogout"
+        if(logoutOrDelete === "delete") return "postDelete"
+        return "postLogout"
+    }
+
+    testAPI[getRequestByLogoutOrDelete()]()
+        .then(response => {
+            if(response.status !== 200)
+                throw `CANNOT FETCH POST REQUEST ${response.status}`
+            return response.json()
+        })
+        .then( ({result = false})  => {
+            if(result) {
+                dispatch(logoutOrDeleteUseAC())
+                dispatch(push("/feed"))
+            }
+        })
+        .catch(err => console.error(err))
+}
+
+//--------------------ANNOUNCEMENT-PAGE--------------------//
+type GetAnnouncementByCategoryAndIdDispatchType = Dispatch<SettIsFetchingAnnouncementReducerACType | SetAnnouncementsACType>
+export const getAnnouncementByCategoryAndIdThunk = (category:string, id:number) => (dispatch: GetAnnouncementByCategoryAndIdDispatchType) => {
+    dispatch(settIsFetchingAnnouncementReducerAC(true))
+    console.log("getAnnouncementByCategoryAndIdThunk")
+
+    testAPI["getAnnouncementByCategoryAndId"](category, id)
+        .then(response => {
+            if(response.status !== 200)
+                throw `CANNOT FETCH GET REQUEST ${response.status}`
+            return response.json()
+        })
+        .then( (data)  => {
+            dispatch(settIsFetchingAnnouncementReducerAC(false))
+            dispatch(setAnnouncementAC(data))
+        })
+        .catch(err => console.error(err))
+}
+
+//--------------------MY-ANNOUNCEMENT-PAGE--------------------//
+type GetMyAnnouncementsDispatchType = Dispatch<SetMyAnnouncementsACType | SettIsFetchingMyAnnouncementReducerACType | SetTotalNumOfPageMyAnnouncementReducerACType>
+export const getMyAnnouncementsThunk = (withConcat = false) => (dispatch: GetMyAnnouncementsDispatchType, getState: any) => {
+    dispatch(settIsFetchingMyAnnouncementsReducerAC(true))
+    console.log("getMyAnnouncementsThunk")
+
+    const { myAnnouncementsState : { currentPage } } = getState()
+
+    testAPI["getMyAnnouncements"](currentPage)
+        .then(response => {
+            if(response.status !== 200)
+                throw `CANNOT FETCH GET REQUEST ${response.status}`
+            return response.json()
+        })
+        .then( ({totalNumOfPages, announcements}) => {
+            dispatch(settIsFetchingMyAnnouncementsReducerAC(false))
+            dispatch(setTotalNumOfPageMyAnnouncementReducerAC(totalNumOfPages))
+            dispatch(setMyAnnouncementsAC(announcements, withConcat))
+        } )
+        .catch(err => console.error(err))
+}
+
+type PostDeleteAnnouncementDispatchType = Dispatch<SettIsFetchingMyAnnouncementReducerACType | DelMyAnnouncementByIdACType>
+export const postDeleteAnnouncementThunk = (id: number) => (dispatch: PostDeleteAnnouncementDispatchType) => {
+    dispatch(settIsFetchingMyAnnouncementsReducerAC(true))
+    console.log("postDeleteAnnouncementThunk")
+
+    testAPI["postDeleteAnnouncement"](id)
+        .then(response => {
+            if(response.status !== 200)
+                throw `CANNOT FETCH POST REQUEST ${response.status}`
+            return response.json()
+        })
+        .then( ({result = false})  => {
+            dispatch(settIsFetchingMyAnnouncementsReducerAC(false))
+            result && dispatch(delMyAnnouncementByIdAC(id))
+        })
         .catch(err => console.error(err))
 }
