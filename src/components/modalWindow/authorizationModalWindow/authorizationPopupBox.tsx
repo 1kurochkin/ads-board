120681dfeb26c90b295c00f99acccd7e03e14310
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-09 14:03:07 +0300

    Рефакторинг компонента AuthorizationPopupBox - перенос хранения состояния текстовых полей в formState

--- src/components/modalWindow/authorizationModalWindow/authorizationPopupBox.tsx
+++ src/components/modalWindow/authorizationModalWindow/authorizationPopupBox.tsx
@@ -1,153 +1,110 @@
-import React, {ChangeEvent, useCallback, useState} from 'react';
+import React, {ChangeEvent, useCallback, useEffect} from 'react';
 import "./authorizationPopupBoxStyles.css"
 import TextInput from "../../textInput/textInput";
 import {useDispatch, useSelector} from "react-redux";
 import {sendAuthorizationOrRegistrationThunk} from "../../../redux/thunks/thunks";
 import {
-    setIsCorrectAuthDataAC, setIsRegistrationAC,
+    setIsCorrectAuthDataAC,
+    setIsRegistrationAC,
 } from "../../../redux/reducers/authorizationState/authorizationStateActionCreators";
-import {getIsCorrectAuthDataSelector, getIsRegistrationSelector} from "../../../redux/reducers/authorizationState/authorizationStateSelectors";
-import {getIsSendingDataSelector} from "../../../redux/reducers/mainState/mainStateSelectors";
-import { AuthorizationData } from '../../../api/testAPI';
+import {
+    getIsCorrectAuthDataSelector,
+    getIsRegistrationSelector
+} from "../../../redux/reducers/authorizationState/authorizationStateSelectors";
+import {getIsFetchingMainStateSelector} from "../../../redux/reducers/mainState/mainStateSelectors";
 import Button from "../../button/button";
+import {
+    checkIsReadyToSendByPageFormReducerAC, resetToInitialByPageFormReducerAC,
+    setIsValidFormReducerAC,
+    seValueFormReducerAC
+} from "../../../redux/reducers/formState/formStateActionCreators";
+import {getFieldsByPageFormReducerSelector} from "../../../redux/reducers/formState/formStateSelectors";
+import {prepareFormStateByPageForSend} from "../../../redux/reducers/formState/formState";
 
-type loginOrPassOrNameTypes = "password" | "login" | "name" | string
+
+type fieldTypes = "password" | "login" | "name" | string
 
 type PropsType = {
     closeModalWindow?: Function
 }
 
 const AuthorizationPopupBox = (props: PropsType) => {
-    //-----LOCAL-STATE-----//
-    const [state, setLogin] =
-        useState({
-            name: {value: "", isValid: true},
-            login: {value: "", isValid: true},
-            password: {value: "", isValid: true}
-        })
 
     //------MAP-STATE-TO-PROPS-----//
-    const {isRegistration, isSendingData, isCorrectAuthData} = useSelector((state) => ({
-        isRegistration: getIsRegistrationSelector(state),
-        isCorrectAuthData: getIsCorrectAuthDataSelector(state),
-        isSendingData: getIsSendingDataSelector(state)
-    }))
+    const isRegistration = useSelector(getIsRegistrationSelector)
+    const isCorrectAuthData = useSelector(getIsCorrectAuthDataSelector)
+    const isFetching = useSelector(getIsFetchingMainStateSelector)
+    const authFormState: any = useSelector((state) => getFieldsByPageFormReducerSelector(state, "authorization"))
+    const regFormState: any = useSelector((state) => getFieldsByPageFormReducerSelector(state, "registration"))
+    const currentFormState = (isRegistration ? regFormState : authFormState) || {}
+    const currentPage = isRegistration ? "registration" : "authorization"
+    const {isReadyToSend} = currentFormState
 
     //-----MAP-DISPATCH-TO-PROPS----//
     const dispatch = useDispatch()
     const sendAuthorizationOrRegistration = useCallback((data) => dispatch(sendAuthorizationOrRegistrationThunk(data)), [dispatch])
     const setIsRegistration = useCallback((boolean) => dispatch(setIsRegistrationAC(boolean)), [dispatch])
+    const seValueFormReducer = useCallback((value, field) => dispatch(seValueFormReducerAC(value, field, currentPage)), [dispatch, isRegistration])
     const setIsCorrectAuthData = useCallback(() => dispatch(setIsCorrectAuthDataAC(true)), [dispatch])
+    const setIsValidFormReducer = useCallback((field) => dispatch(setIsValidFormReducerAC(field, currentPage)), [dispatch, isRegistration])
+    const resetToInitialByPageFormReducer = useCallback((page) => dispatch(resetToInitialByPageFormReducerAC(page)), [dispatch, isRegistration])
+    const checkIsReadyToSend = useCallback(() => dispatch(checkIsReadyToSendByPageFormReducerAC(currentPage)), [dispatch, isRegistration])
 
-    //Функция - setter, для установки состояния value.
-    const setStateValue = (value: string, loginOrPassOrName: loginOrPassOrNameTypes) => {
-        console.log("setStateValue", value, loginOrPassOrName)
-        setLogin((prevState: any) => {
-            const prevLoginOrPass = prevState[loginOrPassOrName]
-            return {...prevState, [loginOrPassOrName]: {...prevLoginOrPass, value}}
-        })
-    }
-
-    //Функция - setter, для установки состояния isValid.
-    const setStateIsValid = (isValid: boolean, loginOrPassOrName: loginOrPassOrNameTypes) => {
-        console.log("setStateIsValid", isValid, loginOrPassOrName)
-        setLogin((prevState: any) => {
-            const prevLoginOrPass = prevState[loginOrPassOrName]
-            return {...prevState, [loginOrPassOrName]: {...prevLoginOrPass, isValid}}
-        })
-    }
-
-    //Функция для проверки валидности значения ввёдного в инпут.
-    const checkIsValid = (value: string, loginOrPass: loginOrPassOrNameTypes) => {
-        console.log("checkIsValid", value, loginOrPass)
-        switch (loginOrPass) {
-            case "name":
-                return !!value.length
-            case "login":
-                return !!value.length
-            case "password":
-                return !!value.length
-            default:
-                return true
+    //------WILL-UNMOUNT-LIFE-CYCLE-----//
+    useEffect(() => {
+        return () => {
+            resetToInitialByPageFormReducer("authorization")
+            resetToInitialByPageFormReducer("registration")
         }
-    }
+    }, [])
 
-    //Функция для проверки готовности отправки запроса на сервер.
-    const checkIsReadyToSend = () => {
-        console.log("checkIsReadyToSend")
-        const stateEntries = Object.entries(state)
-        const isValidsArray = stateEntries.map(([loginOrPassOrName, {value}]) => {
-            const isValid = checkIsValid(value, loginOrPassOrName)
-            console.log(loginOrPassOrName, isValid, value, "checkIsReadyToSend MAP")
-            !isValid && setStateIsValid(false, loginOrPassOrName)
-            return isValid
-        })
-        // @ts-ignore
-        return !isValidsArray.includes(false)
-    }
-
-    //Функция - обработчик события изменеия в инпуте. Проверка на валидность значения в инпуте.
-    const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>, loginOrPass: loginOrPassOrNameTypes) => {
-        console.log("onChangeHandler", loginOrPass)
-        // @ts-ignore
-        const {isValid} = state[loginOrPass]
-        const {currentTarget: {value}} = event
-        !isValid && checkIsValid(value, loginOrPass) ?
-            setStateIsValid(true, loginOrPass) :
-            setStateValue(value, loginOrPass)
-        !isCorrectAuthData && setIsCorrectAuthData()
-    }
-
-    //Функция - обработчик события блур. Проверка на валидность значения в инпуте,
-    // а так-же, если блур в поле логин, то отправка запроса на существование пользователя
-    const onBlurHandler = (event: FocusEvent, loginOrPassOrName: loginOrPassOrNameTypes) => {
-        console.log("onBlurHandler", loginOrPassOrName)
-        // @ts-ignore
-        const {value} = state[loginOrPassOrName]
-        const isValid = checkIsValid(value, loginOrPassOrName)
-        setStateIsValid(isValid, loginOrPassOrName)
-    }
-
-    //Функция - обработчик события клик по кнопке отправить или зарегестрироваться
-    const onClickHandler = () => {
-        console.log("onClickHandler", checkIsReadyToSend())
-        if(checkIsReadyToSend()) {
-            const postData: AuthorizationData = Object.entries(state).reduce( (obj: any, [loginOrPassOrName, {value}]) => {
-                if(!isRegistration && loginOrPassOrName == "name") return obj
-                return obj[loginOrPassOrName] = value
-            }, {} )
+    //------DID-UPDATE-LIFE-CYCLE-----//
+    useEffect(() => {
+        if(isReadyToSend) {
+            const postData = prepareFormStateByPageForSend(currentFormState)()
             sendAuthorizationOrRegistration(postData)
         }
+    }, [isReadyToSend])
+
+    //Функция - обработчик события изменеия в инпуте. Проверка на валидность значения в инпуте.
+    const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>, field: fieldTypes) => {
+        const {currentTarget: {value}} = event
+        console.log("onChangeHandler", field, value)
+        seValueFormReducer(value, field)
+        !isCorrectAuthData && setIsCorrectAuthData()
     }
 
     //Функция возращает массив с конфигурацией для полей ввода
     const getInputsConfig = () => {
-        const {
-            name: {value: loginName, isValid: isValidName},
-            login: {value: loginValue, isValid: isValidLogin},
-            password: {value: passwordValue, isValid: isValidPassword}
-        } = state
+        const {name = {}, login = {}, password = {}, phoneNumber = {}} = currentFormState
         return [
             {
-                loginOrPassOrName: "name",
+                field: "name",
                 inputType: "text",
                 placeholder: "Введите ваше имя",
-                value: loginName,
-                isValid: isValidName
+                value: name.value,
+                isValid: name.isValid
             },
             {
-                loginOrPassOrName: "login",
+                field: "phoneNumber",
+                inputType: "number",
+                placeholder: "Введите ваш телефон",
+                value: phoneNumber.value,
+                isValid: phoneNumber.isValid
+            },
+            {
+                field: "login",
                 inputType: "text",
                 placeholder: "Введите ваш E-mail",
-                value: loginValue,
-                isValid: isValidLogin
+                value: login.value,
+                isValid: login.isValid
             },
             {
-                loginOrPassOrName: "password",
+                field: "password",
                 inputType: "password",
                 placeholder: "Введите ваш пароль",
-                value: passwordValue,
-                isValid: isValidPassword
+                value: password.value,
+                isValid: password.isValid
             },
         ]
     }
@@ -163,18 +120,19 @@ const AuthorizationPopupBox = (props: PropsType) => {
         <div className="authorization-popupBox">
             <h1 className={"authorization-popupBox__title"}>{!isRegistration ? "Вход" : "Регистрация"}</h1>
             <div className="authorization-popupBox__inputs-wrapper">
-                { getInputsConfig().map( ({loginOrPassOrName, inputType, placeholder, value, isValid}) => {
-                    if(!isRegistration && loginOrPassOrName == "name") return null
-                    return <TextInput key={loginOrPassOrName}
+                { getInputsConfig().map( ({field, inputType, placeholder, value, isValid}) => {
+                    if(!isRegistration && field == "name") return null
+                    if(!isRegistration && field == "phoneNumber") return null
+                    return <TextInput key={field}
                                       isValid={isValid} placeholder={placeholder}
                                       inputType={inputType} value={value}
-                                      onBlurHandler={(event: FocusEvent) => onBlurHandler(event, loginOrPassOrName)}
-                                      onChangeHandler={(event: ChangeEvent<HTMLInputElement>) => onChangeHandler(event, loginOrPassOrName)}/>
+                                      onBlurHandler={() => setIsValidFormReducer(field)}
+                                      onChangeHandler={(event: ChangeEvent<HTMLInputElement>) => onChangeHandler(event, field)}/>
                 }) }
             </div>
             {!isCorrectAuthData && !isRegistration && <h4>Введённые данные не верны</h4>}
-            <Button onClickHandler={onClickHandler} label={!isRegistration ? "Войти" : "Зарегестрироваться"}
-                    isDisabled={isSendingData} className={"authorization-popupBox__btn"}/>
+            <Button onClickHandler={checkIsReadyToSend} label={!isRegistration ? "Войти" : "Зарегестрироваться"}
+                    isDisabled={isFetching} className={"authorization-popupBox__btn"}/>
             {getBtnByIsExistUserForSwitch()}
         </div>
     );
