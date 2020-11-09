34f5c95bb11dd0c1552a71f172c40529fb995cc6
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-09 13:44:59 +0300

    Создал ButtonUp компонент

---
+++ src/components/buttonUp/buttonUp.tsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+import "./buttonUpStyles.css"
+import Button from '../button/button';
+
+
+const ButtonUp = (props: any) => {
+
+    const [isView, setIsView] = useState(false)
+
+    useEffect(() => {
+        document.addEventListener("scroll", onScrollHandler)
+        return () => document.removeEventListener("scroll", onScrollHandler)
+    }, [])
+
+    const onScrollHandler = (event: Event) => {
+        const currentHeight = Math.ceil(window.innerHeight + window.scrollY)
+        const innerHeight = window.innerHeight
+        currentHeight < innerHeight + 300 ? setIsView(false) : setIsView(true)
+    }
+
+  return isView ?
+      <Button className={"btn-up"} label={"Наверх"}
+              onClickHandler={() => window.scrollTo(0,0)} /> : null
+}
+
+export default ButtonUp;
