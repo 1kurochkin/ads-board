de15367729b8cb967a3de6cb0c387e3cb8837069
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-10 14:22:35 +0300

    Updates

---
+++ precache-manifest.a20fcd99012db8a2f150124fa2f15aef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab9495c19d9ba80017ce3629dbb7fba",
+    "url": "/ads-board/index.html"
+  },
+  {
+    "revision": "58546f123965396a7283",
+    "url": "/ads-board/static/css/main.552ba9d0.chunk.css"
+  },
+  {
+    "revision": "f33fffa07c49e3a270fc",
+    "url": "/ads-board/static/js/2.7953e484.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ads-board/static/js/2.7953e484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58546f123965396a7283",
+    "url": "/ads-board/static/js/main.3dd5e765.chunk.js"
+  },
+  {
+    "revision": "b3117e15f66f6da51f11",
+    "url": "/ads-board/static/js/runtime-main.a68f9f92.js"
+  },
+  {
+    "revision": "d73128e40f0b4b719939f8cd6a01d30a",
+    "url": "/ads-board/static/media/defaultAvatar.d73128e4.jpg"
+  }
+]);
