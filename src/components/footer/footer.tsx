e19efe48496cdd10606d52e350f650aa2921fa2f
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-12 19:46:01 +0300

    Cтили для Footer поправил навигацию

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 import "./footerStyles.css"
 import { NavLink } from 'react-router-dom';
 import Logo from "../logo/logo";
+import {PATH_CREATE_ANNOUNCEMENT} from "../../app/App";
+import NavCategoryButton from "../navCategoryButton/navCategoryButton";
+import {useSelector} from "react-redux";
+import {getCategoriesDataSelector} from "../../redux/reducers/mainState/mainStateSelectors";
 
 const Footer = (props: any) => {
 
-  const linkConfigs = [
-      {path: "/contacts", label: "Контакты"},
-      {path: "/contacts", label: "Реклама"},
-      {path: "/contacts", label: "Поддержка"}
-  ]
+    //------MAP-STATE-TO-PROPS-----//
+    const categoriesData = useSelector(getCategoriesDataSelector)
 
+    //TODO Вынести в компонент navigation
   return (
-      <div className="footer">
+      <div className="navbar navbar-dark bg-info pl-5 pr-5">
           <Logo/>
-          <div className="footer__navigation-wrapper">
-            {linkConfigs.map( ({path, label}) =>
-              <NavLink className="footer__navigation-link" to={path}>
-                {label}
-              </NavLink> )}
+          <div className="navbar-nav flex-row">
+              {categoriesData.map((categoryData: any, index:number) => {
+                  const {category, label, id} = categoryData
+                  return index !== 0 &&
+                      <NavCategoryButton className={"nav-link ml-3"} key={id} category={category} configCategory={categoryData}>
+                          {label}
+                      </NavCategoryButton> })}
           </div>
-          <NavLink className="header__btn-link btn" to={"/createAnnouncement"}>
+          <NavLink className="btn btn-outline-light" to={PATH_CREATE_ANNOUNCEMENT}>
               Разместить объявление
           </NavLink>
       </div>
