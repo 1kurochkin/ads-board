78cadf6410acaa56f9ec6ac6f23dc9610ffa693f
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-12 19:47:59 +0300

    Добавил событие onClick для компонента Image

--- src/components/image/image.tsx
+++ src/components/image/image.tsx
@@ -4,13 +4,14 @@ import "./imageStyles.css"
 type ImagePropsType = {
     photo: string
     className?: string
+    onClickHandler?: any
 }
 
 const Image = (props: ImagePropsType) => {
 
-    const {photo, className} = props
+    const {photo, className, onClickHandler = null} = props
 
-  return <div style={{backgroundImage: `url(${photo})`}} className={`image ${className}`}/>
+  return <div onClick={onClickHandler} style={{backgroundImage: `url(${photo})`}} className={`image ${className}`}/>
 }
 
 export default Image;
