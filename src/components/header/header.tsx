68e7b3417d636df9a42ec9b9246ba4719d208071
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-12 19:47:17 +0300

    Cтили для Header поправил навигацию

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,16 +3,16 @@ import "./headerStyles.css"
 import {Link, NavLink} from 'react-router-dom';
 import Logo from "../logo/logo";
 import AuthorizationModalWindow from "../modalWindow/authorizationModalWindow/authorizationModalWindow";
-import {PATH_CREATE_ANNOUNCEMENT, PATH_HOUSING, PATH_JOB, PATH_MY_ANNOUNCEMENTS, PATH_SETTINGS} from "../../app/App";
+import {PATH_CREATE_ANNOUNCEMENT, PATH_MY_ANNOUNCEMENTS, PATH_SETTINGS} from "../../app/App";
 import {useDispatch, useSelector} from "react-redux";
-import {
-    getIsAuthSelector,
-} from "../../redux/reducers/authorizationState/authorizationStateSelectors";
+import {getIsAuthSelector,} from "../../redux/reducers/authorizationState/authorizationStateSelectors";
 import AlertModalWindow from "../modalWindow/alertModalWindow/alertModalWindow";
 import Button from "../button/button";
-import {postLogoutOrDeleteUser, postSettingByFieldThunk} from "../../redux/thunks/thunks";
+import {postLogoutOrDeleteUser} from "../../redux/thunks/thunks";
 import useOutsideClick from "../../hooks/useOutsideClick";
 import {getSettingsFieldValueByFieldSelector} from "../../redux/reducers/settingsState/settingsStateSelectors";
+import {getCategoriesDataSelector} from "../../redux/reducers/mainState/mainStateSelectors";
+import NavCategoryButton from "../navCategoryButton/navCategoryButton";
 
 const Header = (props: any) => {
 
@@ -22,6 +22,7 @@ const Header = (props: any) => {
     //------MAP-STATE-TO-PROPS-----//
     const isAuth = useSelector(getIsAuthSelector)
     const name = useSelector((state) => getSettingsFieldValueByFieldSelector(state, "name"))
+    const categoriesData = useSelector(getCategoriesDataSelector)
 
     //-----MAP-DISPATCH-TO-PROPS----//
     const dispatch = useDispatch()
@@ -43,13 +44,13 @@ const Header = (props: any) => {
         setIsActiveAuthMenu(prevState => !prevState)
     }
 
-    const linkConfigs = [
-        {path: PATH_HOUSING, label: "Недвижимость"},
-        {path: PATH_JOB, label: "Работа"}
-    ]
+    // const linkConfigs = [
+    //     {path: PATH_HOUSING, label: "Недвижимость"},
+    //     {path: PATH_JOB, label: "Работа"}
+    // ]
 
     const alertCreateAnnouncement = useCallback(() =>
-        <AlertModalWindow openBtnElement={<Button className={"header__btn-link"} label={"Разместить объявление"}/>}
+        <AlertModalWindow openBtnElement={<Button className={"header__btn-link btn-outline-light"} label={"Разместить объявление"}/>}
                           btnOneConfiguration={{btnOneLabel: "Позже", btnOneHandler:() => setIsActiveAuthModal(false) }}
                           btnTwoConfiguration={{btnTwoLabel: "Авторизироваться", btnTwoHandler:() => setIsActiveAuthModal(true) }}
                           alertText={"Для размещения объявления, необходимо авторизироваться!"}/>, [])
@@ -61,35 +62,38 @@ const Header = (props: any) => {
 
 
     const linkToCreateAnnouncement = <>
-        <Link className="header__btn-link btn" to={PATH_CREATE_ANNOUNCEMENT}>
+        <NavLink activeClassName={"active"} className="header__btn-link btn btn-outline-light" to={PATH_CREATE_ANNOUNCEMENT}>
             Разместить объявление
-        </Link>
+        </NavLink>
     </>
 
     const authMenu = <>
-        <div ref={ref} onClick={toggleIsActiveAuthMenu} className="header__authMenu">
-            <h3 className="header__authMenu-name">{name}</h3>
-            <div className={`header__authMenu-menu ${isActiveAuthMenu && "header__authMenu-menu-active"}`}>
-                <Link to={PATH_MY_ANNOUNCEMENTS} className="header__authMenu-menu-item">
+        <div ref={ref} onClick={toggleIsActiveAuthMenu} className="dropdown">
+            <Button className={"btn-warning dropdown-toggle"} label={name}/>
+            <div className={`dropdown-menu ${isActiveAuthMenu && "show"}`}>
+                <Link to={PATH_MY_ANNOUNCEMENTS} className="dropdown-item">
                     Мои объявления
                 </Link>
-                <Link to={PATH_SETTINGS} className="header__authMenu-menu-item">
+                <Link to={PATH_SETTINGS} className="dropdown-item">
                     Настройки
                 </Link>
-                <div onClick={() => setIsActiveLogoutAlert(true)} className="header__authMenu-menu-item">Выход</div>
+                <div className="dropdown-divider"/>
+                <div onClick={() => setIsActiveLogoutAlert(true)} className="dropdown-item">Выход</div>
             </div>
         </div>
     </>
 
     return (
-        <div className="header">
-            <div className="header__inner-wrapper">
+        <div className="navbar navbar-dark bg-info pl-5 pr-5">
+            <div className="d-flex">
                 <Logo/>
-                <div className="header__navigation-wrapper">
-                    {linkConfigs.map(({path, label}) =>
-                        <NavLink className="header__navigation-link" to={path}>
+                <div className="header__navigation-wrapper navbar-nav flex-row ml-5">
+                    {categoriesData.map((categoryData: any, index:number) => {
+                        const {category, label, id} = categoryData
+                        return index !== 0 &&
+                            <NavCategoryButton className={"nav-link ml-3"} key={id} category={category} configCategory={categoryData}>
                             {label}
-                        </NavLink>)}
+                        </NavCategoryButton> })}
                 </div>
             </div>
             <div className="header__inner-wrapper">
