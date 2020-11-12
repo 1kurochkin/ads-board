7598949425c471582a78be9be647e35e80af3dd9
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-12 20:17:37 +0300

    Стили для Announcement

--- src/components/announcement/announcement.tsx
+++ src/components/announcement/announcement.tsx
@@ -12,26 +12,31 @@ type AnnouncementPropsType = {
     creationDate: string
     subwayStation: string
     hasLike: boolean
+    className?: string
 }
 
 const Announcement = (props: AnnouncementPropsType) => {
 
-    const {photo, name, creationDate, category, price, subwayStation, id} = props
+    const {photo, name, creationDate, category, className, price, subwayStation, id} = props
+
 
   return (
-      <div className="announcement">
-          <NavLink to={`${category}/${id}`}>
-              <div className="announcement__photo-wrapper">
+      <div className={`border text-center text-white border-dark mb-3 mr-3 p-0 ${className}`}>
+              <NavLink to={`${category}/${id}`}>
+          <div className="card-header p-0">
                   <Image photo={photo}/>
-                  {/*<div style={{backgroundImage: `url(${photo})`}} className="announcement__photo"/>*/}
-              </div>
-          </NavLink>
-          <div className="announcement__info-wrapper">
-              <h3 className="announcement__info-name">{name}</h3>
-              <div className="announcement__info-price">{price}</div>
-              <div className="announcement__info-subway">{subwayStation}</div>
-              <div className="announcement__info-creationDate">{creationDate}</div>
           </div>
+              </NavLink>
+              <div className={"card-inner-wrapper d-flex flex-column justify-content-between"}>
+                  <div className="card-body d-flex flex-column justify-content-between">
+                      <h5 className="card-title text-primary">{name}</h5>
+                      <h6 className="card-text text-dark">{`Цена: ${price} руб.`}</h6>
+                      <h6 className="card-text text-dark">{`Метро: ${subwayStation}`}</h6>
+                  </div>
+                  <div className="card-footer">
+                      <small className="text-muted">{`Дата создания: ${creationDate}`}</small>
+                  </div>
+              </div>
       </div>
   )
 }
