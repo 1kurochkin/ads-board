f43b6a83f194a66339d578ccaae3d5e4583abf45
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-12 21:34:03 +0300

    Рефакторинг навигации в компоненте footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import "./footerStyles.css"
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import Logo from "../logo/logo";
-import {PATH_CREATE_ANNOUNCEMENT} from "../../app/App";
-import NavCategoryButton from "../navCategoryButton/navCategoryButton";
-import {useSelector} from "react-redux";
-import {getCategoriesDataSelector} from "../../redux/reducers/mainState/mainStateSelectors";
+import {PATH_CONTACTS, PATH_CREATE_ANNOUNCEMENT} from "../../app/App";
 
 const Footer = (props: any) => {
 
-    //------MAP-STATE-TO-PROPS-----//
-    const categoriesData = useSelector(getCategoriesDataSelector)
+    const linkConfigs = [
+        {path: PATH_CONTACTS, label: "Контакты"},
+        {path: PATH_CONTACTS, label: "Реклама"},
+        {path: PATH_CONTACTS, label: "Поддержка"}
+    ]
+
 
     //TODO Вынести в компонент navigation
   return (
       <div className="navbar navbar-dark bg-info pl-5 pr-5">
           <Logo/>
           <div className="navbar-nav flex-row">
-              {categoriesData.map((categoryData: any, index:number) => {
-                  const {category, label, id} = categoryData
-                  return index !== 0 &&
-                      <NavCategoryButton className={"nav-link ml-3"} key={id} category={category} configCategory={categoryData}>
-                          {label}
-                      </NavCategoryButton> })}
+            {linkConfigs.map( ({path, label}) =>
+              <NavLink className="nav-link ml-3" to={path}>
+                {label}
+              </NavLink> )}
           </div>
           <NavLink className="btn btn-outline-light" to={PATH_CREATE_ANNOUNCEMENT}>
               Разместить объявление
