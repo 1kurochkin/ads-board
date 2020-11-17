a8be22165651a89b6835d84ee512fa27a5e525f1
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-17 22:32:18 +0300

    Стили для Footer + рефакторинг

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,31 +1,19 @@
 import React from 'react';
 import "./footerStyles.css"
-import {NavLink} from 'react-router-dom';
 import Logo from "../logo/logo";
-import {PATH_CONTACTS, PATH_CREATE_ANNOUNCEMENT} from "../../app/App";
+import {linkToCreateAnnouncement} from "../../app/App";
+import NavBar from "../navBar/navBar";
 
 const Footer = (props: any) => {
 
-    const linkConfigs = [
-        {path: PATH_CONTACTS, label: "Контакты"},
-        {path: PATH_CONTACTS, label: "Реклама"},
-        {path: PATH_CONTACTS, label: "Поддержка"}
-    ]
-
-
-    //TODO Вынести в компонент navigation
   return (
-      <div className="navbar navbar-dark bg-info pl-5 pr-5">
-          <Logo/>
-          <div className="navbar-nav flex-row">
-            {linkConfigs.map( ({path, label}) =>
-              <NavLink className="nav-link ml-3" to={path}>
-                {label}
-              </NavLink> )}
+      <div className="navbar navbar-dark bg-info flex-md-column flex-lg-row pl-5 pr-5 flex-column justify-content-center flex-md-row">
+          <Logo className={"mb-3 mb-lg-0"}/>
+          <div className="d-flex flex-column flex-lg-row flex-fill col-sm-12 col-md-10 flex-md-column align-items-md-center flex-md-row p-0 justify-content-md-around">
+              <NavBar/>
+              {linkToCreateAnnouncement("mt-4 mt-lg-0")}
           </div>
-          <NavLink className="btn btn-outline-light" to={PATH_CREATE_ANNOUNCEMENT}>
-              Разместить объявление
-          </NavLink>
+
       </div>
   );
 }
