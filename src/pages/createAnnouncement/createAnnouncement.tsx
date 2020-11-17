d21d5590a01be16c5152d33524f2850129ccd493
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-17 22:28:52 +0300

    Заменил наименования полей связанных с formState на странице CreateAnnouncement + рефакторинг

--- src/pages/createAnnouncement/createAnnouncement.tsx
+++ src/pages/createAnnouncement/createAnnouncement.tsx
@@ -11,7 +11,7 @@ import Image from "../../components/image/image";
 import {
     getCategoriesDataSelector,
     getIsFetchingMainStateSelector,
-    getSubwayStationsDataSelector
+    getSubwayStationsDataSelector, getTheSubCategoriesSelector
 } from "../../redux/reducers/mainState/mainStateSelectors";
 import {
     checkIsReadyToSendByPageFormReducerAC, resetToInitialByPageFormReducerAC,
@@ -23,6 +23,7 @@ import {prepareFormStateByPageForSend} from "../../redux/reducers/formState/form
 import {getSettingsFieldValueByFieldSelector} from "../../redux/reducers/settingsState/settingsStateSelectors";
 import withAuthRedirectHoc from "../../hocs/withAuthRedirectHoc";
 import ImagePicker from "../../components/imagePicker/imagePicker";
+import AlertModalWindow from "../../components/modalWindow/alertModalWindow/alertModalWindow";
 
 type CreateAnnouncementFieldsType =
     "photos"
@@ -38,14 +39,15 @@ const CreateAnnouncement = (props: any) => {
 
     //------MAP-STATE-TO-PROPS-----//
     const subwayStationsData = useSelector(getSubwayStationsDataSelector)
-    const categoriesData = useSelector(getCategoriesDataSelector)
+    const [defaultCategory, ...categoriesData] = useSelector( (state) =>
+        getTheSubCategoriesSelector(getCategoriesDataSelector(state), "active"))
     const phoneRedux = useSelector( (state) => getSettingsFieldValueByFieldSelector(state, "phone"))
     const formState = useSelector((state) => getFieldsByPageFormReducerSelector(state, "createAnnouncement"))
     const {
-        photos, name,
-        price, category,
+        photoList, name,
+        price, categoryId,
         description,
-        phone, subway,
+        sellerPhone, stationId,
         isReadyToSend
     } = formState
     const isFetching = useSelector(getIsFetchingMainStateSelector)
@@ -53,21 +55,24 @@ const CreateAnnouncement = (props: any) => {
     //-----MAP-DISPATCH-TO-PROPS----//
     const dispatch = useDispatch()
     const postNewAnnouncement = useCallback((data) => dispatch(postNewAnnouncementThunk(data)), [dispatch])
-    const seValueFormReducer = useCallback((value, field) => dispatch(seValueFormReducerAC(value, field, "createAnnouncement")), [dispatch])
+    const setValueFormReducer = useCallback((value, field) => dispatch(seValueFormReducerAC(value, field, "createAnnouncement")), [dispatch])
     const setIsValidFormReducer = useCallback((field) => dispatch(setIsValidFormReducerAC(field, "createAnnouncement")), [dispatch])
     const checkIsReadyToSend = useCallback(() => dispatch(checkIsReadyToSendByPageFormReducerAC("createAnnouncement")), [dispatch])
     const resetToInitialByPageFormReducer = useCallback(() => dispatch(resetToInitialByPageFormReducerAC("createAnnouncement")), [dispatch])
 
     //------DID-MOUNT-LIFE-CYCLE-----//
     useEffect(() => {
-        seValueFormReducer(phoneRedux, "phone")
+        setValueFormReducer(phoneRedux, "sellerPhone")
+        return () => {
+            resetToInitialByPageFormReducer()
+        }
     }, [])
 
     //------DID-UPDATE-LIFE-CYCLE-----//
     useEffect(() => {
         if(isReadyToSend) {
             const condition = (postData: any, key: any, value: any) => {
-                key === "category" || key === "subway" ? postData[key] = value.id : postData[key] = value
+                key === "categoryId" || key === "stationId" ? postData[key] = value.id : postData[key] = value
                 return postData
             }
             const postData = prepareFormStateByPageForSend(formState)(condition)
@@ -75,26 +80,27 @@ const CreateAnnouncement = (props: any) => {
         }
     }, [isReadyToSend])
 
-    const selectItemOnChangeHandler = (field: "category" | "subway", selectItem: any, setIsActiveSelect: Function) => {
-        seValueFormReducer(selectItem, field)
+    const selectItemOnChangeHandler = (field: "categoryId" | "stationId", selectItem: any, setIsActiveSelect: Function) => {
+        if("className" in selectItem) return false
+        setValueFormReducer(selectItem, field)
         setIsActiveSelect(false)
     }
 
     const onLoadImageHandler = (image: any, imageName: string) => {
-        const value = photos.value.concat({photo: image, name: imageName})
-        seValueFormReducer(value, "photos")
+        const value = photoList.value.concat({photo: image, name: imageName})
+        setValueFormReducer(value, "photoList")
     }
 
     //Функция - обработчик события изменеия в инпуте. Проверка на валидность значения в инпуте.
     const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>, field: CreateAnnouncementFieldsType) => {
         console.log("onChangeHandler", field)
         const {currentTarget: {value}} = event
-        seValueFormReducer(value, field)
+        setValueFormReducer(value, field)
     }
 
     const deleteLoadedImage = (imageName: string) => {
-        const newPhotosValue = photos.value.filter( ({photo, name}: any) => name !== imageName)
-        seValueFormReducer(newPhotosValue, "photos")
+        const newPhotosValue = photoList.value.filter( ({photo, name}: any) => name !== imageName)
+        setValueFormReducer(newPhotosValue, "photoList")
     }
 
     //Функция возращает массив с конфигурацией для полей ввода
@@ -126,7 +132,7 @@ const CreateAnnouncement = (props: any) => {
 
     // const {name, subway, photos, description, phone, category, price} = state
 
-    const getErrorClassName = (field: "category" | "subway") =>
+    const getErrorClassName = (field: "categoryId" | "stationId") =>
         !formState[field].isValid && "createAnnouncement__title-error"
 
     return (
@@ -135,16 +141,16 @@ const CreateAnnouncement = (props: any) => {
             <div className="createAnnouncement__container container-lg pt-5">
                 <h1 className="display-5 jumbotron p-2 mb-5">Создание объявления</h1>
                 <div className="createAnnouncement__category d-flex">
-                    <h4 className={`createAnnouncement__category-title col-lg-3 text-left p-0 ${getErrorClassName("category")}`}>
+                    <h4 className={`createAnnouncement__category-title col-lg-3 text-left p-0 ${getErrorClassName("categoryId")}`}>
                         Категория
                     </h4>
-                    <Select className={"col-lg-4"} onBlurHandler={() => setIsValidFormReducer("category")}
-                        onChangeHandlerSelectItem={(selectItem: any, handler: any) => selectItemOnChangeHandler("category", selectItem, handler)}
-                        value={category.value.label} selectItems={categoriesData} placeHolder={"Выбор категории"}/>
+                    <Select className={"col-lg-4"} onBlurHandler={() => setIsValidFormReducer("categoryId")}
+                        onChangeHandlerSelectItem={(selectItem: any, handler: any) => selectItemOnChangeHandler("categoryId", selectItem, handler)}
+                        value={categoryId.value.label} selectItems={categoriesData} placeHolder={"Выбор категории"}/>
                 </div>
                 <hr className="my-4"/>
-                <div className="createAnnouncement__params d-flex">
-                    <h4 className="createAnnouncement__params-title col-lg-3 text-left p-0">Параметры</h4>
+                <div className="createAnnouncement__params d-block d-lg-flex">
+                    <h4 className="createAnnouncement__params-title col-lg-3 text-lg-left p-0 text-center">Параметры</h4>
                     <div className="col-lg-4 p-0">
                         {getInputsParamsConfig().map(({field, ...restConfig}) =>
                             <TextInput className={"mb-3"} key={field} {...restConfig}
@@ -152,37 +158,46 @@ const CreateAnnouncement = (props: any) => {
                                        onChangeHandler={(event: ChangeEvent<HTMLInputElement>) => onChangeHandler(event, field)}/>)}
 
                         <div className="createAnnouncement__params-photos position-relative">
-                            <div className="createAnnouncement__params-photos-label mb-3">
-                                <span>{`Фотографии ${photos.value.length} из 5`}</span>
-                            </div>
-                            <div style={{height: "73px"}} className="createAnnouncement__params-photos-files d-flex">
-                                {photos.value.map( ({photo, name}: any) => <Image onClickHandler={() => deleteLoadedImage(name)} className={"createAnnouncement__params-photos-files-file col-lg-4 mr-2"} photo={photo}/> )}
-                                {photos.value.length < 5 &&
-                                <ImagePicker className={"col-lg-4 p-0"} onLoadHandler={onLoadImageHandler}/>}
+                            {/*<div className="createAnnouncement__params-photos-label mb-3">*/}
+                                <span className={"createAnnouncement__params-photos-label mb-3"}>
+                                    {`Фотографии ${photoList.value.length} из 5`}
+                                </span>
+                            {/*</div>*/}
+                            <div className="createAnnouncement__params-photos-files d-flex flex-wrap">
+                                {photoList.value.map( ({photo, name}: any) =>
+                                    <Image onClickHandler={() => deleteLoadedImage(name)} className={"createAnnouncement__params-photos-files-file col-lg-4 mr-2"} photo={photo}/> )}
+                                {photoList.value.length < 5 &&
+                                <ImagePicker className={"col-lg-4 p-0 my-3"} onLoadHandler={onLoadImageHandler}/>}
+                                <span>
+                                    Чтобы удалить фото - нажмите на него
+                                </span>
                             </div>
                         </div>
                     </div>
                 </div>
                 <hr className="my-4"/>
                 <div className="createAnnouncement__location d-flex">
-                    <h4 className={`createAnnouncement__location-title col-lg-3 text-left p-0 ${getErrorClassName("subway")}`}>Место сделки</h4>
-                    <Select className={"col-lg-4 p-0"} onBlurHandler={() => setIsValidFormReducer("subway")}
-                        onChangeHandlerSelectItem={(selectItem: any, handler: any) => selectItemOnChangeHandler("subway", selectItem, handler)}
-                        value={subway.value.label} selectItems={subwayStationsData} placeHolder={"Выбор метро"}/>
+                    <h4 className={`createAnnouncement__location-title col-lg-3 text-left p-0 ${getErrorClassName("stationId")}`}>Место сделки</h4>
+                    <Select className={"col-lg-4 p-0"} onBlurHandler={() => setIsValidFormReducer("stationId")}
+                        onChangeHandlerSelectItem={(selectItem: any, handler: any) => selectItemOnChangeHandler("stationId", selectItem, handler)}
+                        value={stationId.value.name} selectItems={subwayStationsData} placeHolder={"Выбор метро"}/>
                 </div>
                 <hr className="my-4"/>
 
                 <div className="createAnnouncement__contacts d-flex">
                     <h4 className="createAnnouncement__contacts-title col-lg-3 text-left p-0">Контакты</h4>
-                    <TextInput className={"col-lg-3 p-0"} isValid={phone.isValid} value={phone.value}
+                    <TextInput className={"col-lg-3 p-0"} isValid={sellerPhone.isValid} value={sellerPhone.value}
                                placeholder={"Номер телефона"} label={"Телефон"} inputType={"number"}
-                               onBlurHandler={() => setIsValidFormReducer("phone")}
-                               onChangeHandler={(event: ChangeEvent<HTMLInputElement>) => onChangeHandler(event, "phone")}/>
+                               onBlurHandler={() => setIsValidFormReducer("sellerPhone")}
+                               onChangeHandler={(event: ChangeEvent<HTMLInputElement>) => onChangeHandler(event, "sellerPhone")}/>
                 </div>
                 <hr className="my-4"/>
-                <div className={"d-flex justify-content-around mb-5 col-lg-7 p-0"}>
-                    <Button className={"btn-success col-lg-7"} label={"Создать объявление"} isDisabled={isFetching} onClickHandler={checkIsReadyToSend}/>
-                    <Button className={"btn-danger col-lg-4"} label={"Очистить поля"} isDisabled={isFetching} onClickHandler={resetToInitialByPageFormReducer}/>
+                <div className={"d-lg-flex justify-content-around mb-5 col-lg-7 p-0 d-block"}>
+                    <Button className={"btn-success col-lg-7 my-3 my-lg-0"} label={"Создать объявление"} isDisabled={isFetching} onClickHandler={checkIsReadyToSend}/>
+                    <AlertModalWindow openBtnElement={<Button className={"btn-danger col-lg-4"} label={"Очистить поля"} />}
+                                      btnOneConfiguration={{btnOneLabel: "Нет" }}
+                                      btnTwoConfiguration={{btnTwoLabel: "Да", btnTwoHandler:resetToInitialByPageFormReducer }}
+                                      alertText={"Сбросить все введённые данные?"}/>
                 </div>
             </div>
             <Footer/>
