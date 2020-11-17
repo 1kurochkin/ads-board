9539960040be934c67f981010d9a7572464499bf
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-17 22:33:31 +0300

    Стили для Logo + изменил сам компонент

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -3,11 +3,12 @@ import "./logoStyles.css"
 import { NavLink } from 'react-router-dom';
 
 const Logo = (props: any) => {
-
+    const {className} = props
   return (
-      <NavLink to={"/feed"}>
-          <div className="logo navbar-brand mb-0 h1">
-              SALAM
+      <NavLink className={className} to={"/feed"}>
+          <div className="logo navbar-brand mb-0 h1 d-flex">
+              <span className={"border rounded d-flex align-items-center p-0 pl-2 pr-2 mr-2 font-weight-bold bg-warning text-dark border-dark"}>C</span>
+              <span className={"font-weight-bold"}>calam.ru</span>
           </div>
       </NavLink>
 
