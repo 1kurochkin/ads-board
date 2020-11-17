3bac02b53977090886d922739143233752c3f7c2
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-17 22:05:38 +0300

    Добавил case RESET_TO_DEFAULT_STATE_ALL_REDUCERS и конкатенацию в SET_LAST_ANNOUNCEMENTS, feedStateReducer

--- src/redux/reducers/feedState/feedState.tsx
+++ src/redux/reducers/feedState/feedState.tsx
@@ -1,3 +1,5 @@
+import {RESET_TO_DEFAULT_STATE_ALL_REDUCERS} from "../mainState/mainState";
+
 export const SET_IS_FETCHING_FEED_REDUCER = "SET_IS_FETCHING_FEED_REDUCER"
 export const SET_LAST_ANNOUNCEMENTS = "SET_LAST_ANNOUNCEMENTS"
 
@@ -10,15 +12,20 @@ const initialState = {
 
 export const feedStateReducer = (state = initialState, action: any): initialStateType => {
     const {type, payload } = action
-    const { value } = payload || {}
+    const { value, withConcat = false } = payload || {}
+    const {lastAnnouncements} = state
 
     switch (type) {
         case SET_LAST_ANNOUNCEMENTS :
             console.log("SET_LAST_ANNOUNCEMENTS", value)
-            return {...state, lastAnnouncements: value}
+            const newLastAnnouncements = withConcat ? lastAnnouncements.concat(value) : value
+            return {...state, lastAnnouncements: newLastAnnouncements}
         case SET_IS_FETCHING_FEED_REDUCER :
             console.log("SET_IS_FETCHING_FEED_REDUCER", value)
             return {...state, isFetching: value}
+        case RESET_TO_DEFAULT_STATE_ALL_REDUCERS :
+            console.log("RESET_TO_DEFAULT_STATE_ALL_REDUCERS", value)
+            return initialState
         default: return state
     }
 }
--- src/redux/reducers/feedState/feedStateActionCreators.tsx
+++ src/redux/reducers/feedState/feedStateActionCreators.tsx
@@ -1,7 +1,7 @@
 import { SET_LAST_ANNOUNCEMENTS, SET_IS_FETCHING_FEED_REDUCER } from "./feedState"
 
-export type SetLastAnnouncementsACType = { type: typeof SET_LAST_ANNOUNCEMENTS, payload: { value: boolean }}
+export type SetLastAnnouncementsACType = { type: typeof SET_LAST_ANNOUNCEMENTS, payload: { value: Array<any> , withConcat:boolean}}
 export type SetetIsFetchingFeedReducerACType = { type: typeof SET_IS_FETCHING_FEED_REDUCER, payload: { value: boolean }}
 
-export const setLastAnnouncementsAC = (value:boolean):SetLastAnnouncementsACType => ({ type: SET_LAST_ANNOUNCEMENTS, payload: {value} })
+export const setLastAnnouncementsAC = (value:Array<any>, withConcat:boolean):SetLastAnnouncementsACType => ({ type: SET_LAST_ANNOUNCEMENTS, payload: {value, withConcat} })
 export const setIsFetchingFeedReducerAC = (value:boolean):SetetIsFetchingFeedReducerACType => ({ type: SET_IS_FETCHING_FEED_REDUCER, payload: {value} })
