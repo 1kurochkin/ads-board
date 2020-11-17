2b3b313856168424c8d5772376ffa58b49635cd7
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-17 22:13:31 +0300

    Добавил props styles, в комопнент ImagePicker

--- src/components/imagePicker/imagePicker.tsx
+++ src/components/imagePicker/imagePicker.tsx
@@ -4,11 +4,12 @@ import "./imagePickerStyles.css"
 type ImagePickerPropsType = {
     onLoadHandler: Function
     className?: string
+    styles?: any
 }
 
 const ImagePicker = (props: ImagePickerPropsType) => {
 
-    const {onLoadHandler, className} = props
+    const {onLoadHandler, className, styles=null} = props
 
     const onChangeFileHandler = (event: any) => {
         const file = event.target.files[0]
@@ -23,7 +24,7 @@ const ImagePicker = (props: ImagePickerPropsType) => {
         file && reader.readAsDataURL(file)
     }
 
-  return <div className={`h-100 w-100 ${className}`}>
+  return <div style={styles} className={`h-100 w-100 ${className}`}>
       <input onChange={onChangeFileHandler} type="file" className={`imagePicker`}/>
       <div className="h-100 w-100 d-flex justify-content-center align-items-center border border-dark alert-dark">
          <h2 className={"display-4"}>+</h2>
