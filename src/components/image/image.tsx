3be309527e0fab96299df36a5d7413ffd59f625b
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-17 22:34:08 +0300

    Добавил porps styles для комопннета Image

--- src/components/image/image.tsx
+++ src/components/image/image.tsx
@@ -5,13 +5,14 @@ type ImagePropsType = {
     photo: string
     className?: string
     onClickHandler?: any
+    styles?: any
 }
 
 const Image = (props: ImagePropsType) => {
 
-    const {photo, className, onClickHandler = null} = props
+    const {photo, className, onClickHandler = null, styles={}} = props
 
-  return <div onClick={onClickHandler} style={{backgroundImage: `url(${photo})`}} className={`image ${className}`}/>
+  return <div onClick={onClickHandler} style={{backgroundImage: `url(${photo})`, ...styles}} className={`image ${className}`}/>
 }
 
 export default Image;
