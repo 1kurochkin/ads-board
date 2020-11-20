0b68256ef2d2a9d34226c86da4063b5a67deef7c
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-20 20:09:43 +0300

    Изменило логотип Logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -7,8 +7,8 @@ const Logo = (props: any) => {
   return (
       <NavLink className={className} to={"/feed"}>
           <div className="logo navbar-brand mb-0 h1 d-flex">
-              <span className={"border rounded d-flex align-items-center p-0 pl-2 pr-2 mr-2 font-weight-bold bg-warning text-dark border-dark"}>C</span>
-              <span className={"font-weight-bold"}>calam.ru</span>
+              <span className={"border rounded d-flex align-items-center p-0 pl-2 pr-2 mr-2 font-weight-bold bg-warning text-dark border-dark"}>S</span>
+              <span className={"font-weight-bold"}>salam.ru</span>
           </div>
       </NavLink>
 
