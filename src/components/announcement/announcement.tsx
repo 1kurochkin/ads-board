208150ba4fd02e41f28cbb8a234b497a41f5a138
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-23 01:12:47 +0300

    исправил отображение фотографий в Announcement

--- src/components/announcement/announcement.tsx
+++ src/components/announcement/announcement.tsx
@@ -5,7 +5,7 @@ import Image from "../picture/picture";
 import {GET_PATH_SEARCH} from "../../app/App";
 
 type AnnouncementPropsType = {
-    photo: string
+    photos: string
     id: number
     name: string
     category: string
@@ -18,14 +18,14 @@ type AnnouncementPropsType = {
 
 const Announcement = (props: AnnouncementPropsType) => {
 
-    const {photo, name, creationDate, category, className, price, station, id} = props
+    const {photos, name, creationDate, category, className, price, station, id} = props
 
 
   return (
       <div className={`border text-center d-flex flex-column flex-md-row text-white border-dark mb-3 mr-3 p-0 ${className}`}>
               <NavLink className={"col-lg-4 col-md-5 col-sm-12 p-0 card-header"} to={`${GET_PATH_SEARCH(category)}/${id}`}>
           {/*<div className="card-header p-0">*/}
-                  <Image photo={photo}/>
+                  <Image photo={photos[0]}/>
           {/*</div>*/}
               </NavLink>
               <div className={"card-inner-wrapper d-flex flex-column justify-content-between"}>
