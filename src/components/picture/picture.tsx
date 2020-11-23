efeea22fd537a5848abbc7ee258723bc92a0d451
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-23 14:50:57 +0300

    Улучшение Picture, добавил условия для отображения фотографии, в зависимости от источника фото (сервер, загруженное изображение), а так же отслеживание загрузки фото и ошибки при загрузке

--- src/components/picture/picture.tsx
+++ src/components/picture/picture.tsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./pictureStyles.css"
-import useBadCaseImage from "../../hooks/useBadCaseImage";
 import badCaseImage from "../../pictures/badCaseImage.png";
 import Preloader from "../preloader/preloader";
+import {API_URL} from "../../api/serverAPI";
 
 type PicturePropsType = {
-    photo: string
+    photo: string | null | any
     className?: string
     onClickHandler?: any
     styles?: any
@@ -13,22 +13,34 @@ type PicturePropsType = {
 
 const Picture = (props: PicturePropsType) => {
 
-    const {photo, className, onClickHandler = null, styles={}} = props
+    const {photo = "", className, onClickHandler = null, styles={}} = props
+    const isImageFromServer = !photo.includes("data")
 
-    //
-    // const [imageSRC, setImageSRC] = useState(photo)
-    const [isLoadImage, setIsLoadImage] = useState(true)
+    const getImageSrc = () => isImageFromServer ? `${API_URL}/photo/${photo}` : photo
 
-    // const image = new Image
-    // image.src = imageSRC
-    // image.onload = () => {
-    //     console.log("LOAD IMAGE")
-    // }
-    // image.onerror = () => setImageSRC(badCaseImage)
+    const [imageSRC, setImageSRC] = useState(() => "")
+    const [isLoadImage, setIsLoadImage] = useState(!isImageFromServer)
+
+    useEffect(() => {
+        setImageSRC(getImageSrc())
+        if(isImageFromServer && photo) {
+            const image = new Image
+            image.src = getImageSrc()
+            image.onload = () => setIsLoadImage(true)
+            image.onerror = () => {
+                setIsLoadImage(true)
+                setImageSRC(badCaseImage)
+            }
+        }
+        if(!photo) {
+            setImageSRC(badCaseImage)
+            setIsLoadImage(true)
+        }
+    }, [photo])
 
   return isLoadImage ?
       <div onClick={onClickHandler}  className={`image ${className}`}
-           style={{backgroundImage: `url(${photo})`, ...styles}} /> :
+           style={{backgroundImage: `url(${imageSRC})`, ...styles}} /> :
       <Preloader/>
 }
 
