c015c9c75366f15a549341972117071f7d6562ab
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-28 15:51:47 +0300

    bad case для пропса price Announcement

--- src/components/announcement/announcement.tsx
+++ src/components/announcement/announcement.tsx
@@ -5,7 +5,7 @@ import Image from "../picture/picture";
 import {GET_PATH_SEARCH} from "../../app/App";
 
 type AnnouncementPropsType = {
-    photos: string
+    photos: Array<string>
     id: number
     name: string
     categoryName: string
@@ -18,18 +18,20 @@ type AnnouncementPropsType = {
 
 const Announcement = (props: AnnouncementPropsType) => {
 
-    const {photos, name, creationDate, categoryName, className, price, station, id} = props
+    const {photos = [], name, creationDate, categoryName, className, price, station, id} = props
 
 
   return (
       <NavLink className={`announcement border cursor-pointer text-center d-flex flex-column flex-md-row text-white border-dark mb-3 mr-3 p-0 ${className}`} to={`${GET_PATH_SEARCH(categoryName)}/${id}`}>
           <div className="col-lg-4 col-md-5 p-0 card-header">
-                  <Image photo={photos[0]}/>
+              {!photos[0] ? <div className="h-100 w-100 d-flex align-items-center justify-content-center bg-light border-right">
+                  <h6 className="card-text text-dark">{`Метро: ${station}`}</h6>
+              </div> : <Image photo={photos[0]}/>}
           </div>
               <div className={"card-inner-wrapper d-flex flex-column justify-content-between"}>
                   <div className="card-body d-flex flex-column justify-content-between">
                       <h5 className="card-title text-primary">{name}</h5>
-                      <h6 className="card-text text-dark">{`Цена: ${price} руб.`}</h6>
+                      {price !== 0 && <h6 className="card-text text-dark">{`Цена: ${price} руб.`}</h6>}
                       <h6 className="card-text text-dark">{`Метро: ${station}`}</h6>
                   </div>
                   <div className="card-footer">
