2bcd430aa52ebec02720060311aa75053a2934fc
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-29 22:43:11 +0300

    Добавил паддинг для ModalWindow

--- src/components/modalWindow/modalWindow.tsx
+++ src/components/modalWindow/modalWindow.tsx
@@ -35,7 +35,7 @@ const ModalWindow = (props: propsTypes) => {
 
         <>
             <div className={`modalWindow ${isActive && "modalWindow__active"} `}>
-                <div ref={ref} className="p-lg-5 p-4 bg-light position-relative">
+                <div ref={ref} className="p-lg-5 p-4 bg-light position-relative pt-5">
                     {React.cloneElement(modal, {closeModalWindow})}
                     <span onClick={() => closeModalWindow()} className={"popup__close"}>&times;</span>
                 </div>
