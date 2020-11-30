7b8798ccb9673b9862f7b2fc746557099a43dcfe
i.kurochkin <i.kurochkin@simplex-software.ru>
2020-11-30 23:37:06 +0300

    Изменил логотп на salamkg.ru

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -8,7 +8,7 @@ const Logo = (props: any) => {
       <NavLink className={className} to={"/feed"}>
           <div className="logo navbar-brand mb-0 h1 d-flex">
               <span className={"border rounded d-flex align-items-center p-0 pl-2 pr-2 mr-2 font-weight-bold bg-warning text-dark border-dark"}>S</span>
-              <span className={"font-weight-bold"}>salam.ru</span>
+              <span className={"font-weight-bold"}>salamkg.ru</span>
           </div>
       </NavLink>
 
