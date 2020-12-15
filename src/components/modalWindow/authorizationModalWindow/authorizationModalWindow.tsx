c6c1646646252ce1fd2d44065b67cbda34ce7e9a
Payting App <paytingapp@gmail.com>
2020-12-15 22:43:46 +0300

    Добавил пропс в AuthorizationModalWindow

--- src/components/modalWindow/authorizationModalWindow/authorizationModalWindow.tsx
+++ src/components/modalWindow/authorizationModalWindow/authorizationModalWindow.tsx
@@ -6,15 +6,16 @@ import Button from "../../button/button";
 type AuthorizationModalWindowType = {
   isActiveFromProps?: boolean
   alertCloseHandler?:Function
+  classNameForBtn?: string
 }
 const AuthorizationModalWindow = (props: AuthorizationModalWindowType) => {
 
-  const {isActiveFromProps = false, alertCloseHandler} = props
+  const {isActiveFromProps = false, alertCloseHandler, classNameForBtn} = props
 
   return (
       <ModalWindow alertCloseHandler={alertCloseHandler} isActiveFromProps={isActiveFromProps} withOpenBtn={true} modal={<AuthorizationPopupBox/>}>{
           (openModalWindow:any) =>
-              <Button svgIconName={"login"} label="Вход и регистрация" onClickHandler={openModalWindow} className="btn-outline-light createAnnouncement__link align-items-end mr-md-3 mb-3 mb-md-0"/>
+              <Button svgIconName={"login"} label="Вход и регистрация" onClickHandler={openModalWindow} className={`btn-outline-light createAnnouncement__link align-items-end mr-md-3 mb-3 mb-md-0 ${classNameForBtn}`}/>
       }</ModalWindow>
   );
 }
